1a5c31b0e10409a515b8b6734e36a1caa8d5aeec
Noah Yarian <noah.yarian@gmail.com>
2015-06-15 15:43:58 -0500

    halfway to gulpville

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,66 @@
 var gulp = require('gulp'),
-    $    = require('gulp-load-plugins')();
+    $    = require('gulp-load-plugins')({
+      pattern: ['gulp-*', 'del', 'wiredep', 'main-bower-files']
+    });
 
-gulp.task('build', function () {
+gulp.task('clean', function () {
+  $.del('public');
+});
+
+gulp.task('bower', function () {
+  gulp
+    .src($.mainBowerFiles('**/*.js'))
+    .pipe($.concat('build.js'))
+    .pipe(gulp.dest('public/lib'));
+});
+
+gulp.task('jade:dev', function () {
   gulp
     .src(['src/**/*.jade', '!src/**/_*.jade'])
     .pipe($.jade({
       pretty: true
     }))
     .pipe(gulp.dest('public'));
-
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('jade:prod', function () {
+  gulp
+    .src(['src/**/*.jade', '!src/**/_*.jade'])
+    .pipe($.jade())
+    .pipe(gulp.dest('public'));
 });
+
+gulp.task('sass:dev', function () {
+  gulp
+    .src('src/_styles/main.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('sass:prod', function () {
+  gulp
+    .src('src/_styles/main.scss')
+    .pipe($.sass({
+      outputStyle: 'compressed'
+      })
+      .on('error', $.sass.logError)
+    )
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('build', ['clean', 'jade:prod', 'sass:prod']);
+
+gulp.task('serve', function () {
+    gulp.watch(['src/**/*.jade'], ['jade:dev'])
+    gulp.watch(['src/**/*.scss'], ['sass:dev'])
+
+    gulp.watch(['src/**/*'], ['build'])
+});
+
+
+
+
+
+gulp.task('default', function() {});
