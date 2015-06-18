453718c5299db6faa0467f94129ae71c65215961
Noah Yarian <noah.yarian@gmail.com>
2015-06-18 16:46:19 -0500

    fixed gulp clean with callback. gulp (dev) looking good

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
     $    = require('gulp-load-plugins')({
-      pattern: ['gulp-*', 'del', 'wiredep', 'main-bower-files', 'browser-sync']
+      pattern: ['gulp-*', 'del', 'main-bower-files', 'browser-sync']
     });
 
-gulp.task('clean', function () {
-  $.del('public');
+gulp.task('clean', function (cb) {
+  $.del('public', cb);
 });
 
 gulp.task('bower', function () {
@@ -27,12 +27,14 @@ gulp.task('jade:dev', function () {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('jade:prod', function () {
-  gulp
-    .src(['src/**/*.jade', '!src/**/_*.jade'])
-    .pipe($.jade())
-    .pipe(gulp.dest('public'));
-});
+// gulp.task('jade:prod', function () {
+//   gulp
+//     .src(['src/**/*.jade', '!src/**/_*.jade'])
+//     .pipe($.sourcemaps.init())
+//     .pipe($.jade())
+//     .pipe($.sourcemaps.write())
+//     .pipe(gulp.dest('public'));
+// });
 
 gulp.task('sass:dev', function () {
   gulp
@@ -42,22 +44,22 @@ gulp.task('sass:dev', function () {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('sass:prod', function () {
-  gulp
-    .src('src/_styles/main.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      outputStyle: 'compressed'
-      })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['> 1%'],
-      cascade: true
-    }))
-    //.pipe($.minifyCss({compatibility: 'ie8'}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('public/css'));
-});
+// gulp.task('sass:prod', function () {
+//   gulp
+//     .src('src/_styles/main.scss')
+//     .pipe($.sourcemaps.init())
+//     .pipe($.sass({
+//       outputStyle: 'compressed'
+//       })
+//       .on('error', $.sass.logError))
+//     .pipe($.autoprefixer({
+//       browsers: ['> 1%'],
+//       cascade: true
+//     }))
+//     //.pipe($.minifyCss({compatibility: 'ie8'}))
+//     .pipe($.sourcemaps.write())
+//     .pipe(gulp.dest('public/css'));
+// });
 
 gulp.task('js:dev', function () {
   gulp.src('src/**/*.js')
@@ -65,37 +67,13 @@ gulp.task('js:dev', function () {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('js:prod', function () {
-  gulp.src('src/**/*.js')
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.uglify())
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('public'));
-});
-
-// gulp.task('uglify', function() {
+// gulp.task('js:prod', function () {
 //   gulp.src('src/**/*.js')
-//     .pipe(uglify())
+//     .pipe($.sourcemaps.init())
+//     .pipe($.babel())
+//     .pipe($.uglify())
+//     .pipe($.sourcemaps.write())
 //     .pipe(gulp.dest('public'));
-// });
-
-// gulp.task('autoprefixer', function () {
-//   gulp.src('public/css/main.css')
-//     .pipe($.autoprefixer({
-//       browsers: ['> 1%'],
-//       cascade: false
-//     }))
-//     .pipe(gulp.dest('public/css'));
-// });
-
-// gulp.task('concatcss', function() {
-//   var cssFiles = ['/bower_components/**/*'];
-//   gulp.src($.mainBowerFiles().concat(cssFiles))
-//     .pipe($.filter('*.css'))
-//     .pipe($.concat('build.css'))
-//     //.pipe($.uglify())
-//     .pipe(gulp.dest('public/lib'));
 // });
 
 gulp.task('browser-sync', function() {
@@ -106,22 +84,20 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('build:prod', ['jade:prod', 'sass:prod', 'js:prod', 'bower']);
+gulp.task('copy', function () {
+  gulp.src('src/CNAME')
+    .pipe(gulp.dest('public'));
+});
 
-
+//gulp.task('build:prod', ['jade:prod', 'sass:prod', 'js:prod', 'bower', 'copy']);
 gulp.task('build:dev', ['jade:dev', 'sass:dev', 'js:dev', 'bower']);
 
 gulp.task('serve', ['build:dev'], function () {
-
-    // gulp.watch(['src/**/*.jade'], ['jade:dev'])
-    // gulp.watch(['src/**/*.scss'], ['sass:dev'])
-    // gulp.watch(['src/**/*.js'], ['js:dev'])
-
-    gulp.watch(['src/**/*'], ['build:dev']).on('change', $.browserSync.reload);
-});
-
-gulp.task('cleana', ['clean'], function() {
-  gulp.start('build:dev');
+  gulp.start('browser-sync');
+  gulp.watch(['src/**/*.jade'], ['jade:dev']).on('change', $.browserSync.reload);
+  gulp.watch(['src/**/*.scss'], ['sass:dev']).on('change', $.browserSync.reload);
+  gulp.watch(['src/**/*.js'], ['js:dev']).on('change', $.browserSync.reload);
+ // gulp.watch(['src/**/*', '!src/**/*.jade', '!src/**/*.scss', '!src/**/*.js'], ['build:dev']).on('change', $.browserSync.reload);
 });
 
 gulp.task('default', ['clean'], function () {
